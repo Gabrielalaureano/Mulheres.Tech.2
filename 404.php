5d1ad970cabd94cbce779cc651d631d4ac055163
debylu <GabrielacostadeFaria@gmail.com>
2022-10-31 16:48:48 -0300

    abordagem 2- atualizada

---
+++ 404.php
@@ -0,0 +1,35 @@
+<?php
+
+// Importa o arquivo de configuração:
+require('config.php');
+
+/***********************************************
+ * Todo o código PHP desta página começa aqui! *
+ ***********************************************/
+
+// Define o título da página:
+$page_title = 'Erro 404';
+
+/************************************************
+ * Todo o código PHP desta página termina aqui! *
+ ************************************************/
+
+// Importa cabeçalho do tema:
+require('header.php');
+
+/********************************************************
+ * Todo o conteúdo VISUAL da página (HTML) começa aqui! *
+ ********************************************************/
+?>
+
+<h2>Ooooops!</h2>
+<p> A pagina que você tentou acessar não está disponível</p>
+
+<?php
+/*********************************************************
+ * Todo o conteúdo VISUAL da página (HTML) termina aqui! *
+ *********************************************************/
+
+// Importa rodapé do tema:
+require('footer.php');
+?>
